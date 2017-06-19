364ad37240d4c4939ace93254b8acce2b7101891
d3fineme <felipe-alves-09@hotmail.com>
2017-06-19 17:39:19 -0300

    Gráfico
    Implementado os gráficos de estrutura capital

--- home/home.php
+++ home/home.php
@@ -14,6 +14,7 @@
 		<link rel="stylesheet" href="assets/css/main.css" />
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+
 	</head>
 	<body>
 
@@ -22,9 +23,31 @@
 
 				<!-- Main -->
 					<div id="main">
-
+						<center><h3>Índices de Estrutura Capital</h3></center>
+						<div class="row"> <!-- 4 + 4 + 4 = 12 -->
+    						<div class="4u 12u$(small)">
+								<div><?php include "pct.php"; ?></div>
+							</div>
+							<div class="4u 12u$(small)">
+								<div><?php include "end.php"; ?></div>
+							</div>
+							<div class="4u 12u$(small)">
+								<div><?php include "ipl.php"; ?></div>
+							</div>
+						</div>
+						<center><h3>Índices de Liquidez</h3></center>
+						<div class="row"> <!-- 4 + 4 + 4 = 12 -->
+    						<div class="4u 12u$(small)">
+								<div><?php include "pct.php"; ?></div>
+							</div>
+							<div class="4u 12u$(small)">
+								<div><?php include "end.php"; ?></div>
+							</div>
+							<div class="4u 12u$(small)">
+								<div><?php include "pct.php"; ?></div>
+							</div>
+						</div>
                         <iframe src="../contabilidade/indices.php" style="border: 0; width: 100%; height: 100%"></iframe>
-
 					</div>
 
 				<!-- Sidebar -->
---
+++ home/ipl.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+
+<html lang="pt-BR">
+<head>
+      <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['line']});
+      google.charts.setOnLoadCallback(drawChart);
+
+    function drawChart() {
+
+      var data = new google.visualization.DataTable();
+      data.addColumn('number', 'Período');
+      data.addColumn('number', '');
+      
+      data.addRows([
+        [1,  0.37],
+        [2,  0.3],
+        [3,  0.25],
+        [4,  0.11],
+        [5,  0.11],
+        [6,   0.8],
+        [7,   0.7],
+        [8,  0.12],
+        [9,  0.16],
+        [10, 0.12],
+        [11,  0.5],
+        [12,  0.6],
+        [13,  0.4],
+        [14,  0.4]
+      ]);
+
+      var options = {
+        chart: {
+          title: 'IPL',
+        },
+        width: 390,
+        height: 200
+      };
+
+      var chart = new google.charts.Line(document.getElementById('ipl'));
+
+      chart.draw(data, google.charts.Line.convertOptions(options));
+    }
+    </script>
+</head>
+
+<body>    
+  <div id="ipl" style="width: 900px; height: 500px"></div>
+</body>
+
+</html>
+
